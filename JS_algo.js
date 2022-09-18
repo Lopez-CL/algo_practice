8467bc0d9b7eb2d25860a20f9b32e5b1dc4277a7
Lopez-CL <cllopez511@gmail.com>
2022-09-18 18:41:06 -0400

    practicing algos

--- JS_algo.js
+++ JS_algo.js
@@ -9,39 +9,39 @@
 // }
 // console.log(print1To255());
 
-printIntsAndSum0To255()
-Print integers from 0 to 255, and with each integer print the sum so far. 
+// printIntsAndSum0To255()
+// Print integers from 0 to 255, and with each integer print the sum so far. 
 
 
-printArrayVals(arr)
-Iterate through a given array, printing each value. 
+// printArrayVals(arr)
+// Iterate through a given array, printing each value. 
 
-printMaxOfArray(arr)
-Given an array, find and print its largest element. 
+// printMaxOfArray(arr)
+// Given an array, find and print its largest element. 
 
-printAverageOfArray(arr)
-Analyze an array’s values and print the average. 
+// printAverageOfArray(arr)
+// Analyze an array’s values and print the average. 
 
 
-returnOddsArray1To255()
-Create an array with all the odd integers between 1 and 255 (inclusive).  
+// returnOddsArray1To255()
+// Create an array with all the odd integers between 1 and 255 (inclusive).  
 
-squareArrayVals(arr)
-Square each value in a given array, returning that same array with changed values. 
+// squareArrayVals(arr)
+// Square each value in a given array, returning that same array with changed values. 
 
-printArrayCountGreaterThanY(arr, y)
-Given an array and a value Y, count and print the number of array values greater than Y. 
+// printArrayCountGreaterThanY(arr, y)
+// Given an array and a value Y, count and print the number of array values greater than Y. 
 
-zeroOutArrayNegativeVals(arr)
-Return the given array, after setting any negative values to zero. 
+// zeroOutArrayNegativeVals(arr)
+// Return the given array, after setting any negative values to zero. 
 
-printMaxMinAverageArrayVals(arr)
-Given an array, print the max, min and average values for that array.
+// printMaxMinAverageArrayVals(arr)
+// Given an array, print the max, min and average values for that array.
 
-shiftArrayValsLeft(arr)
-Given an array, move all values forward (to the left) by one index, dropping the first value and leaving a 0 (zero) value at the end of the array.
+// shiftArrayValsLeft(arr)
+// Given an array, move all values forward (to the left) by one index, dropping the first value and leaving a 0 (zero) value at the end of the array.
 
-swapStringForArrayNegativeVals(arr)
-Given an array of numbers, replace any negative values with the string 'Dojo'
+// swapStringForArrayNegativeVals(arr)
+// Given an array of numbers, replace any negative values with the string 'Dojo'
 
-//once finished start in the algo book//
+// //once finished start in the algo book//
