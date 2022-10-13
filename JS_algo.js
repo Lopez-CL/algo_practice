e041f76ef909571936cddc67f73d4ac3e3e1ab95
Lopez-CL <cllopez511@gmail.com>
2022-10-13 07:20:05 -0400

    JS algo practice

--- JS_algo.js
+++ JS_algo.js
@@ -124,6 +124,17 @@ console.log(zeroOutArrayNegativeVals([ 1, -4, -9, 16 ]))
 // shiftArrayValsLeft(arr)
 // Given an array, move all values forward (to the left) by one index, dropping the first value and leaving a 0 (zero) value at the end of the array.
 
+function shiftArrayValsLeft(arr){
+    for(var i = 0; i < 4; i++){
+        arr.shift();
+        arr.push(0);
+        console.log(arr);
+    }
+    return arr
+}
+
+console.log(shiftArrayValsLeft([4,3,2,1]))
+
 // swapStringForArrayNegativeVals(arr)
 // Given an array of numbers, replace any negative values with the string 'Dojo'
 
